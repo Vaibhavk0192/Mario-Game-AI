9d2652b3517e52c1feefd286261333f9003484c5
Vaibhavk0192 <vkapoor2_be21@thapar.edu>
2023-11-19 01:19:32 +0530

    making reinforcement model

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 
-env = gym_super_mario_bros.make("SuperMarioBros-v0",apply_api_compatibility=True,render_mode="human")
+env = gym_super_mario_bros.make('SuperMarioBros-v0', apply_api_compatibility=True, render_mode="human")
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
-
 done = True
-for step in range(5000000):
+for step in range(100000):
+# Start the game to begin with
     if done:
-        state=env.reset()
-    state, reward, done, truncated , info = env.step(env.action_space.sample())
-    env.render()
+        env.reset()
+observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
+done = terminated or truncated
+env.render()
 env.close()
 
 
